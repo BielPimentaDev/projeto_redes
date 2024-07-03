5f6aee156f2fb60c399a5da8f315cb857ec23ffc
Gabriel Pimenta <gabrielpimenttas@gmail.com>
2024-07-03 12:02:30 -0300

    first commit

--- public/js/client.js
+++ public/js/client.js
@@ -1,50 +1,54 @@
 let socket = new WebSocket(WS_URL);
-const body = document.querySelector("body");
-const logo = document.getElementById("logo");
-const messageDiv = document.getElementById("message");
+const body = document.querySelector('body');
+const messageDiv = document.getElementById('message');
+const waitingDiv = document.getElementById('waiting');
+const win = document.getElementById('win');
+const loss = document.getElementById('loss');
+const loadDiv = document.getElementById('loadDiv');
 
-socket.addEventListener("message", handleServerMessage);
+socket.addEventListener('message', handleServerMessage);
 
 function handleServerMessage(event) {
-  const data = JSON.parse(event.data);
-  console.log("Mensagem recebida do servidor:", data);
+	const data = JSON.parse(event.data);
+	console.log('Mensagem recebida do servidor:', data);
 
-  switch (data.status) {
-    case STATUS.WIN:
-      setClientState("win", data.code);
-      break;
-    case STATUS.LOSE:
-      setClientState("lose");
-      break;
-  }
+	switch (data.status) {
+		case STATUS.WIN:
+			setClientState('win', data.code);
+			break;
+		case STATUS.LOSE:
+			setClientState('lose');
+			break;
+	}
 }
 
-function setClientState(state, code = "") {
-  // Início da animação
-  body.className = "main";
-  messageDiv.classList.toggle("hide-message", true);
-  logo.classList.toggle("stop-spin", false);
-  logo.classList.toggle("spin-animation", true);
-
-  setTimeout(() => {
-    if (state === "win") {
-      body.classList.add("win");
-      messageDiv.innerText = code;
-      vibratePhone(1000);
-    } else if (state === "lose") {
-      body.classList.add("lose");
-      messageDiv.innerText = "";
-    }
-
-    messageDiv.classList.toggle("show-message", state === "win");
-    messageDiv.classList.toggle("hide-message", state !== "win");
-    logo.classList.toggle("spin-animation", false);
-    logo.classList.toggle("stop-spin", true);
-  }, 4100);
+function setClientState(state, code = '') {
+	messageDiv.classList.toggle('hide-message', true);
+	logo.classList.toggle('stop-spin', false);
+	logo.classList.toggle('spin-animation', true);
+	waitingDiv.style.display = 'none';
+	loadDiv.style.display = 'flex';
+	setTimeout(() => {
+		body.className = 'main';
+		if (state === 'win') {
+			win.style.display = 'block';
+			messageDiv.innerText = code;
+			body.style.backgroundColor = 'rgba(34 197 94)';
+			vibratePhone(1000);
+		} else if (state === 'lose') {
+			body.style.backgroundColor = 'rgba(249 115 22)';
+			loadDiv.style.display = 'none';
+			loss.style.display = 'block';
+			body.messageDiv.innerText = '';
+		}
+		loadDiv.style.display = 'none';
+		messageDiv.classList.toggle('show-message', state === 'win');
+		messageDiv.classList.toggle('hide-message', state !== 'win');
+	}, 2100);
 }
 
 function vibratePhone(timeMs) {
-  if (navigator.vibrate) {
-    navigator.vibrate(timeMs);
-  }
+	if (navigator.vibrate) {
+		navigator.vibrate(timeMs);
+	}
 }
